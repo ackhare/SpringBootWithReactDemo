adee611887d8424243f9cfc3a7216383b025692f
chetan <bairagisirfire@gmail.com>
2017-05-04 15:53:49 +0530

    spring data files

---
+++ src/main/java/com/springReact/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.springReact;
+
+import org.springframework.data.repository.CrudRepository;
+//import org.springframework.security.access.prepost.PreAuthorize;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+//    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @Override
+    void delete(Long aLong);
+
+}
