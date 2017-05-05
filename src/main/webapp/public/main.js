c0838fe40c6d116a8eed37520e8010cce1825e98
chetan <bairagisirfire@gmail.com>
2017-05-05 13:12:22 +0530

    completed updating data

--- src/main/webapp/public/main.js
+++ src/main/webapp/public/main.js
@@ -1,40 +1,22 @@
 var Users = React.createClass({
     componentWillReceiveProps: function () {
-        console.log("run be");
         var self = this;
         if (this.props.fromAjax) {
             $.ajax({
                 url: "http://localhost:8080/api/employees",
             }).then(function (data) {
-                // console.log("gsgsfgsgsfg");
-                // console.log(data)
-
                 self.setState({users: data._embedded.employees});
-
-
             });
         }
     },
 
     componentDidMount: function () {
-        var ref;
-        console.log("Inside component did mount of users ");
-        var self = this;
-        // ref.on('value', function(snap) {
 
-        var mode
-        var items = [];
-        var ref;
         var self = this;
         $.ajax({
             url: "http://localhost:8080/api/employees",
         }).then(function (data) {
-            // console.log("gsgsfgsgsfg");
-            // console.log(data)
-            ref = data._embedded.employees;
             self.setState({users: data._embedded.employees});
-
-
         });
     },
 
@@ -55,12 +37,7 @@ var Users = React.createClass({
 
 
     onNewUser: function (newUser) {
-        // console.log(newUser);
-        // console.log(this.state);
          var self = this;
-        // newUser.key = newUser.name;
-        // ref.push(newUser);
-        // console.log("on new users");
         $.ajax({
             url: "http://localhost:8080/api/employees",
             type: "POST",
@@ -68,7 +45,6 @@ var Users = React.createClass({
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             success: function () {
-               console.log("succ");
                 self.setState({
                     formDisplayed: false
 
@@ -84,11 +60,6 @@ var Users = React.createClass({
     },
 
     render: function () {
-        console.log("in render of users");
-        console.log(this.state.users);
-        console.log(this.props.fromAjax);
-
-        console.log(this.state.users);
         return (
             <div>
                 <UserForm displayed={this.state.formDisplayed} mode_current={'create'} onNewUser={this.onNewUser}/>
@@ -103,17 +74,7 @@ var Users = React.createClass({
 
 var User = React.createClass({
     deleteUser: function () {
-        console.log(this.props.name);
-
-        var name = this.props.name;
         var obj = this.props.users_set;
-        // this.props.users_set.forEach(function (data, index) {
-        //
-        //     if (data.name == name) {
-        //         console.log(index);
-        //         obj.splice(index)
-        //     }
-        // });
         $.ajax({
             url: "http://localhost:8080/api/employees/1",
             type: 'DELETE',
@@ -124,8 +85,6 @@ var User = React.createClass({
                 // you got back from the get request). If successful, display
                 // is set to false and the render is updated. Otherwise,
                 // Toastr notifies the user that an error occurred.
-                console.log('mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm');
-
                 ReactDOM.render(
                     <Users fromAjax={true}/>, document.getElementById('app')
                 );
@@ -134,7 +93,6 @@ var User = React.createClass({
             },
             error: function (xhr, ajaxOptions, thrownError) {
                 //<!--For that you can use Toastr which will allow you to show popups-->
-                console.log("error");
                 console.log(xhr.responseJSON);
                 toastr.error(xhr.responseJSON.message);
             }
@@ -144,10 +102,7 @@ var User = React.createClass({
     },
 
     editUser: function () {
-        console.log('edit user')
         var obj = this.props.users_set;
-
-        console.log(obj);
         ReactDOM.render(
             <UserForm displayed={true} mode_current={'edit'} users_set={obj}
                       user_to_edit={this.props}/>, document.getElementById('app')
@@ -157,8 +112,6 @@ var User = React.createClass({
     render: function () {
 
         return (
-            // <li className="list-group-item">
-
             <tr>
                 <td>{this.props.name}</td>
                 <td>{this.props.age}</td>
@@ -173,15 +126,12 @@ var User = React.createClass({
                     </button>
                 </td>
             </tr>
-            // </li>
         );
     }
 });
 var ShowAddButton = React.createClass({
     render: function () {
-
         var classString, buttonText, button;
-
         if (this.props.displayed) {
             classString = 'btn btn-info ';
             button = 'Cancel';
@@ -201,45 +151,31 @@ var UserForm = React.createClass({
 
     handleForm: function (e) {
         e.preventDefault();
-        console.log('user form');
         if (this.props.mode_current == 'create') {
             var newUser = {
                 name: ReactDOM.findDOMNode(this.refs.name).value,
                 age: ReactDOM.findDOMNode(this.refs.age).value
             };
             ReactDOM.findDOMNode(this.refs.userForm).reset();
-            //this.refs.userForm.getDOMNode().reset();
-
             this.props.onNewUser(newUser);
         }
         else if (this.props.mode_current == 'edit') {
-            console.log('handle edits');
             var existingUser = {
                 name: ReactDOM.findDOMNode(this.refs.name).value,
                 age: ReactDOM.findDOMNode(this.refs.age).value,
                 key: ReactDOM.findDOMNode(this.refs.key).value,
             };
-            console.log(existingUser);
-
         $.ajax({
             url: "http://localhost:8080/api/employees/5",
             type: "PUT",
             data: JSON.stringify({name: existingUser.name, age: existingUser.age}),
             contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function () {
-               console.log("succccccccccccccccccccccccccccccc");
+            dataType: "json"
+        });
 
-            }
-        })
-
-            console.log('editing complete');
-            // console.log(obj2);
             ReactDOM.render(
                 <Users fromAjax={true}/>, document.getElementById('app')
             );
-            // <UsersList users={this.state.users}/>
-            console.log(existingUser);
 
         }
     },
@@ -293,23 +229,10 @@ var UsersList = React.createClass({
 
 
     render: function () {
-//         console.log("render of userList");
-//         console.log(this.props.users);
-// for (var key in this.props.users) {
-//     console.log("fffffffffffffffffff");
-//     console.log(this.props.users[key]);
-//   }
-
         if (!this.props.displayed) {
             console.log("In user list");
             var users_set = this.props.users;
-            // this.props.users.forEach(function(val)
-            // {
-            //     console.log(val.name+"namea");
-            // });
             var userList = this.props.users.map(function (user) {
-                console.log("user");
-                console.log(user);
                 return <User name={user.name} age={user.age} users_set={users_set}/>;
             });
 
@@ -336,13 +259,6 @@ var UsersList = React.createClass({
     }
 
 });
-// var EMPLOYEES = [
-//     {name: 'Joe Biden', age: 45},
-//     {name: 'President Obama', age: 54},
-//     {name: 'Crystal Mac', age: 34},
-//     {name: 'James Henry', age: 33}
-// ];
-
 ReactDOM.render(
     <Users />, document.getElementById('app')
 );
